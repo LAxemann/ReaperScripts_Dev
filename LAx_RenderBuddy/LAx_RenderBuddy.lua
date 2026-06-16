29727d313aa99bdd445c0f7e235c2782f7b41838
Leon Beilmann <Leon.Beilmann@freenet.de>
2026-06-16 22:16:17 +0200

    * v3.20       + Added: $rbregionnumber wildcard. Unlike reaper's $regionnumber, this will assign the actual RenderBuddy region number to the file.       + Removed: "Number regions" setting. Reaper numbers files by default and $rbregionnumber can now be used to place each region's number anywhere.       + Tweaked: Massive backend changes to how rendering works. Please be especially on the lookout for files that don't render where they should or have wrong names.

--- LAx_RenderBuddy/LAx_RenderBuddy.lua
+++ LAx_RenderBuddy/LAx_RenderBuddy.lua
@@ -3,7 +3,7 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 3.19
+ Version: 3.20
  Provides:
   [main] *.lua
   [data] toolbar_icons/**/*.png
@@ -16,10 +16,10 @@
 
 --[[
  * Changelog:
-    * v3.19
-      + Tweaked: The Manager RenderPattern field not features a "$" Wildcards dropdown, allowing to access both Reaper and custom RenderBuddy wildcards 
-      + Tweaked: Manager and WildcardManager inputfields now elide left, leaving the end rather than the start of the string visible
-      + Fixed: Crash when launching any Action without ImGui installed (instead of leading to ImGui installation)
+    * v3.20
+      + Added: $rbregionnumber wildcard. Unlike reaper's $regionnumber, this will assign the actual RenderBuddy region number to the file.
+      + Removed: "Number regions" setting. Reaper numbers files by default and $rbregionnumber can now be used to place each region's number anywhere.
+      + Tweaked: Massive backend changes to how rendering works. Please be especially on the lookout for files that don't render where they should or have wrong names.
 ]] ----------------------------------------------------------------------------------------
 
 SkipLicenseGui = false
--- LAx_Shared/LAx_Shared.lua
+++ LAx_Shared/LAx_Shared.lua
@@ -3,7 +3,7 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 1.26
+ Version: 1.27
  Provides:
   [main] *.lua
   **/*.dat
@@ -14,7 +14,6 @@
 
 --[[
  * Changelog:
-    * v1.25
-      + Tweaked: Validation security tweaks
-      + Tweaked: Cleanup of obsolete/unused functions
+    * v1.27
+      + Added: Various helper functions for filePath management/mutation
 ]]
