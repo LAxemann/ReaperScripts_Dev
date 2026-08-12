e26f96ed103d9a036f0584cce4e93642b6d1f610
Leon Beilmann <Leon.Beilmann@freenet.de>
2026-08-12 12:08:43 +0200

    + Added: Ability to set project default settings in Manager (Manager -> View -> Show project defaults)       + Fixed: Various other, smaller bugfixes

--- LAx_Shared/LAx_Shared.lua
+++ LAx_Shared/LAx_Shared.lua
@@ -3,7 +3,7 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 1.34
+ Version: 1.35
  Provides:
   [main] *.lua
   **/*.dat
@@ -14,7 +14,6 @@
 
 --[[
  * Changelog:
-    * v1.34
-      + Added: LAx_Shared_c_ToolbarManager
+    * v1.35
       + Tweaked: ProductData
 ]]
