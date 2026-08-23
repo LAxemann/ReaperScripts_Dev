a4d9c217bf3e18701865601020cd07ea885a9945
Leon Beilmann <Leon.Beilmann@freenet.de>
2026-08-23 22:59:44 +0200

    Added: user guide

--- LAx_RenderBuddy/LAx_RenderBuddy.lua
+++ LAx_RenderBuddy/LAx_RenderBuddy.lua
@@ -3,11 +3,12 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 3.31
+ Version: 3.32
  Provides:
   [main] *.lua
   [data] toolbar_icons/**/*.png
   [nomain] images/*.png
+  [nomain] *.pdf
   **/*.dat
  About:
   # LAx_RenderBuddy
@@ -16,20 +17,9 @@
 
 --[[
  * Changelog:
-    * v3.31
-      + Added: RenderStats now show the rendered waveform, including preview functionality by clicking and using spacebar
-      + Added: c_MutationJournal now handles and tracks project mutations, allowing it to identify and keep user-intended changes 
-      + Added: Toolbar icons for all actions that were missing them
-      + Tweaked: Made preview track deletion handling more robust
-      + TWeaked: Existing toolbar icons
-      + Fixed: Setting the preview track height to 0 did not actually disable the creation of a preview track
-      + Fixed: The Config Item GUI could save the wrong render format value
-      + Fixed: The manager tooltip example for numbering single files was reversed
-      + Fixed: Double-clicking a specific Config Item would always open the settings of the first Config Item on the track
-      + Fixed: (Project) Wildcards should no longer carry over into new/empty projects and cause trouble 
-      + Fixed: CTRL + A selection now also allows for batch-render selection within the Manager
-      + Fixed: Interplay between setting changes of folder using project defaults and project default settings
-      + Fixed: RenderStats should no longer put info in the wrong line when not normalizing files
+    * v3.32
+      + Added: Trying out pdf inclusion in the archive...
+      + Fixed: An actual track could get deleted instead of the previewTrack in very rare circumstances
 ]] ----------------------------------------------------------------------------------------
 
 SkipLicenseGui = false
--- LAx_Shared/LAx_Shared.lua
+++ LAx_Shared/LAx_Shared.lua
@@ -3,7 +3,7 @@
  Author: Leon 'LAxemann' Beilmann
  REAPER: 6
  Extensions: SWS, JS_ReaScript_API, ReaImGui
- Version: 1.38
+ Version: 1.39
  Provides:
   [main] *.lua
   **/*.dat
@@ -15,6 +15,5 @@
 --[[
  * Changelog:
     * v1.38
-      + Tweaked: More robust license validation framework
       + Tweaked: ProductData
 ]]
